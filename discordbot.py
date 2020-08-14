32d1d3ff6b7d55b40fb0bcc7e985db0e8638621a
tomokura2008 <68816466+tomokura2008@users.noreply.github.com>
2020-08-14 12:37:09 +0900

    discordbot

--- discordbot.py
+++ discordbot.py
@@ -1,38 +1,8 @@
 
 import discord
 from discord.ext import commands
-import typing
-import random
-import time
-import abc
-import rog
-import psutil
-import matplotlib.pyplot as plt
-import asyncio
-import youtube_dl
-import ffmpeg
 from apiclient.discovery import build
-import inspect
-import io
-import textwrap
-import traceback
-import aiohttp
-from contextlib import redirect_stdout
-import base64
-import json
-import os
-import subprocess
-import asyncio
-import itertools
-import sys
-from async_timeout import timeout
-from functools import partial
-import asyncio
-import itertools
-import traceback
-from youtube_dl.utils import lookup_unit_table
-import random
-import playlist
+
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
