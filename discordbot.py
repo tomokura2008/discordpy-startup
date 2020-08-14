e5ac697115c8e960379e04b8334c0d88330d423b
tomokura2008 <68816466+tomokura2008@users.noreply.github.com>
2020-08-14 12:00:31 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,10 @@
+import discord
 from discord.ext import commands
 import os
 import traceback
+import asyncio
 
-bot = commands.Bot(command_prefix='/ms')
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -12,6 +14,25 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+    
+@bot.event
+async def on_ready():
+    bot.session = aiohttp.ClientSession(loop=bot.loop)
+    while True:
+
+        await bot.change_presence(activity=discord.Game(name="herokuで稼働中"))
+        await asyncio.sleep(5)
+        await bot.change_presence(activity=discord.Game(name="更新時間：2020/8/6/21:30"))
+        await asyncio.sleep(5)
+        await bot.change_presence(activity=discord.Game(name="ヘルプ表示/help"))
+        await asyncio.sleep(5)
+        await bot.change_presence(activity=discord.Game(name="目標:"))
+        await asyncio.sleep(5)
+        await bot.change_presence(activity=discord.Game(name="discordbot"))
+        await asyncio.sleep(15)    
+    
+    
+    
 
 @bot.command()
 async def ping(ctx):
